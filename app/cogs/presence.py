9fd274dc5651aaaab5c7d215d2e428bf2b0a4031
ねんねこ <nennneko5787@gmail.com>
2024-07-28 20:57:58 +0900

    presence.py を更新

--- app/cogs/presence.py
+++ app/cogs/presence.py
@@ -10,7 +10,7 @@ class PresenceCog(commands.Cog):
         super().__init__()
         self.bot = bot
 
-    @commands.Cog.listener
+    @commands.Cog.listener()
     async def on_ready(self):
         self.presence.start()
 
